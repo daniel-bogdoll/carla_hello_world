e30ef9461fc83fc78e2ca03f455fde870e417777
Daniel Bogdoll <bogdoll@fzi.de>
2023-11-04 15:51:15 +0100

    Stable, sync, world populated

--- carla_env.py
+++ carla_env.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Environment:
 
-    def __init__(self, world='Town02_Opt', host='localhost', port=2000):
+    def __init__(self, world='Town02_Opt', host='localhost', port=2000, tm_port=8000):
         # Connect to the server
         self.client = carla.Client(host, port)
         TIMEOUT_WAIT=10
@@ -12,9 +12,6 @@ class Environment:
 
         # Load map
         self.world = self.client.load_world(world)
-
-        # Unload map layers https://carla.readthedocs.io/en/latest/core_map/#carla-maps
-        self.world.unload_map_layer(carla.MapLayer.All)
 
         #Get blueprint library and spawn points
         self.bp_lib = self.world.get_blueprint_library()
@@ -31,7 +28,7 @@ class Environment:
         self.client.reload_world(False) # reload map keeping the world settings
 
         # Set up the traffic manager
-        self.TM_PORT = 8000    #https://carla.readthedocs.io/en/latest/adv_traffic_manager/
+        self.TM_PORT = tm_port    #https://carla.readthedocs.io/en/latest/adv_traffic_manager/
         self.traffic_manager = self.client.get_trafficmanager(self.TM_PORT)
         self.traffic_manager.set_synchronous_mode(True)
 
@@ -41,79 +38,76 @@ class Environment:
         random.seed(TM_SEED)
 
         # Tracking all actors
-        self.actor_list = []
+        self.actor_ego = []
+        self.actor_sensors = []
+        self.actor_vehicles = []
 
         print("CARLA initialitaion complete.")
 
     def init_ego(self):
-        BEV_DISTANCE = 10
-        self.IM_WIDTH=1024
-        self.IM_HEIGHT=1024
+        self.RGB_EGO_WIDTH=1024
+        self.RGB_EGO_HEIGHT=1024
+        self.RGB_FOV=105
 
         # Choose vehicle model
         self.vehicle_bp = self.bp_lib.find('vehicle.tesla.model3')
 
-        # Configure semantic segmentation sensor for BEV perspective
-        self.ss_camera_bp = self.bp_lib.find('sensor.camera.semantic_segmentation')
+        # Configure RGB sensor for ego perspective
+        self.rgb_cam_bp = self.bp_lib.find('sensor.camera.rgb')
 
-        self.ss_camera_bp.set_attribute('image_size_x', f'{self.IM_WIDTH}')
-        self.ss_camera_bp.set_attribute('image_size_y', f'{self.IM_HEIGHT}')
-        self.ss_camera_bp.set_attribute('fov', '110')
+        self.rgb_cam_bp.set_attribute('image_size_x', f'{self.RGB_EGO_WIDTH}')
+        self.rgb_cam_bp.set_attribute('image_size_y', f'{self.RGB_EGO_HEIGHT}')
+        self.rgb_cam_bp.set_attribute('fov',  f'{self.RGB_FOV}')
 
-        self.ss_cam_location = carla.Location(0,0,BEV_DISTANCE)
-        self.ss_cam_rotation = carla.Rotation(-90,0,0)
-        self.ss_cam_transform = carla.Transform(self.ss_cam_location, self.ss_cam_rotation)
+        self.rgb_ego_location = carla.Location(2,0,1)
+        self.rgb_ego_rotation = carla.Rotation(-10,0,0)
+        self.rgb_ego_transform = carla.Transform(self.rgb_ego_location,self.rgb_ego_rotation)
 
-        # Configure collision sensor to detect collisions
-        self.col_sensor_bp = self.bp_lib.find('sensor.other.collision')
-
-        self.col_sensor_location = carla.Location(0,0,0)
-        self.col_sensor_rotation = carla.Rotation(0,0,0)
-        self.col_sensor_transform = carla.Transform(self.col_sensor_location, self.col_sensor_rotation)
-
-        # Accessing sensor data
-        self.observation = []
-        self.collision_hist = []
-
-        print("Ego vehicle initialitaion complete.")
-
-    def reset(self):
-        for actor in self.actor_list:
-            actor.destroy()
-
-        self.actor_list = []
-        self.collision_hist = []
+        # Storing sensor data
+        self.rgb_ego = []
 
         # Spawn ego vehicle
         transform = random.choice(self.spawn_points)
         self.vehicle = self.world.spawn_actor(self.vehicle_bp, transform)
-        self.actor_list.append(self.vehicle)
+        self.actor_ego.append(self.vehicle)
 
         # Set autopilot mode
         self.vehicle.set_autopilot(True,self.TM_PORT)
 
-        # Attach and listen to image sensor (BEV Semantic Segmentation)
-        self.ss_cam = self.world.spawn_actor(self.ss_camera_bp, self.ss_cam_transform, attach_to=self.vehicle, attachment_type=carla.AttachmentType.Rigid)
-        self.actor_list.append(self.ss_cam)
-        self.ss_cam.listen(lambda data: self.__process_ss_cam_data(data))
+        # Attach and listen to RGB Ego sensor
+        self.rgb_ego = self.world.spawn_actor(self.rgb_cam_bp, self.rgb_ego_transform, attach_to=self.vehicle, attachment_type=carla.AttachmentType.Rigid)
+        self.actor_sensors.append(self.rgb_ego)
+        self.rgb_ego.listen(lambda data: self.__process_rgb_ego_data(data))
 
-        # Attach and listen to collision sensor
-        self.col_sensor = self.world.spawn_actor(self.col_sensor_bp, self.col_sensor_transform, attach_to=self.vehicle)
-        self.actor_list.append(self.col_sensor)
-        self.col_sensor.listen(lambda event: self.__process_collision_data(event))
-
-        print("Reset complete.")
+        print("Ego vehicle initialitaion complete.")
     
-    def __process_ss_cam_data(self, image):
-        """ Observations directly viewable with OpenCV in CHW format """
-        image.convert(carla.ColorConverter.CityScapesPalette)
-        i = np.array(image.raw_data)
-        i2 = i.reshape((self.IM_HEIGHT, self.IM_WIDTH, 4))
-        i3 = i2[:, :, :3]
-        self.observation = i3
+    def populate(self):
+        NUM_VEHICLES = 50
+        max_vehicles = min([NUM_VEHICLES, len(self.spawn_points)])
 
-    def __process_collision_data(self, event):
-        self.collision_hist.append(event)
+        # Choose the models that should be spawned
+        vehicle_bps = []
+        for vehicle_bp in self.bp_lib.filter('*vehicle*'):
+            vehicle_bps.append(vehicle_bp)
+
+        # Take a random sample of the spawn points and spawn some vehicles
+        for i, spawn_point in enumerate(random.sample(self.spawn_points, max_vehicles)):
+            temp_vehicle = self.world.try_spawn_actor(random.choice(vehicle_bps), spawn_point)
+            if temp_vehicle is not None:
+                self.actor_vehicles.append(temp_vehicle)
+                temp_vehicle.set_autopilot(True,self.TM_PORT)
+        
+        print("World populated.")  
+
+    def __process_rgb_ego_data(self, image):
+        """
+            Observations directly viewable with OpenCV in CHW format.
+            Change this processing if you want to use PyTorch, Pillow, PyGame, Matplotlib, ... 
+         """
+        i = np.array(image.raw_data)
+        i2 = i.reshape((self.RGB_EGO_HEIGHT, self.RGB_EGO_WIDTH, 4))
+        i3 = i2[:, :, :3]
+        self.rgb_ego = i3
 
 
 
--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import cv2
 from carla_env import Environment
 
 def main():
-    env = Environment(world="Town02_Opt", host="172.31.240.1", port=2000)
+    env = Environment(world="Town02_Opt", host="172.31.240.1", port=2000, tm_port=2500)
     env.init_ego()
-    env.reset()
+    env.populate()
+
     try:
         while(True):
             env.world.tick()
             try:
-                cv2.imshow("", env.observation)
+                cv2.imshow("RGB EGO", env.rgb_ego)
                 cv2.waitKey(1)
             except:
                 print("No observation")
     finally:
-        for actor in env.actor_list:
+        cv2.destroyWindow("RGB EGO")
+        for actor in env.actor_ego:
+            actor.destroy()   
+        for actor in env.actor_sensors:
+            actor.destroy()
+        for actor in env.actor_vehicles:
             actor.destroy()   
         print("All actors destroyed")
-        
+
 if __name__ == '__main__':
     main()
